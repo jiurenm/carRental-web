268d566f4b1398b725fadca32f0e0c692b44a7ec
jiurem <44936917+jiurenm@users.noreply.github.com>
2019-03-23 17:42:40 +0800

    Update public.css.d.ts

--- src/css/public.css.d.ts
+++ src/css/public.css.d.ts
@@ -1,4 +1,6 @@
 export const carcousel: string;
+export const hot: string;
 export const login_form: string;
 export const login_form_forgot: string;
 export const login_form_button: string;
+export const ant_list_item: string;
